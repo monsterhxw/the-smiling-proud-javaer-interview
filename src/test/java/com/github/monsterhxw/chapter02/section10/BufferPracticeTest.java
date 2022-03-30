0a794eff867ee66a2ab36f6745c7322eebe6770a
huangxuewei <huangxueweihxw@gmail.com>
2022-03-30 13:10:32 +0800

    add chapter2 buffer practice.

--- src/test/java/com/github/monsterhxw/chapter02/section10/BufferPracticeTest.java
+++ src/test/java/com/github/monsterhxw/chapter02/section10/BufferPracticeTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.*;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.AsynchronousFileChannel;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -22,7 +26,7 @@ class BufferPracticeTest {
     private static final String RESOURCES_PATH = "src/test/resources/section10";
     private static final String DEFAULT_FILE_NAME = "word";
     private static final int BUFFER_SIZE = 4 * 1024;
-    private static final int THRESHOLD = 10_000_000;
+    private static final int THRESHOLD = 1_000_000;
 
     @BeforeAll
     static void setUp() throws IOException {
@@ -93,6 +97,34 @@ class BufferPracticeTest {
         buffer.clear();
         System.out.println("Asynchronous file channel read cost time: " + (System.currentTimeMillis() - startTime) + "ms, character array length is: " + chars.length() + ".");
         channel.close();
+    }
+
+    @Test
+    public void testChinese() {
+        var raw = "长坂桥头杀气生，横枪立马眼圆睁。一声好似轰雷震，独退曹家百万兵。";
+        var utf8Charset = StandardCharsets.UTF_8;
+
+        var bytes = utf8Charset.encode(raw).array();
+        var copyBytes = Arrays.copyOfRange(bytes, 0, 11);
+
+        var byteBuff = ByteBuffer.allocate(12);
+        var charBuff = CharBuffer.allocate(12);
+
+        byteBuff.put(copyBytes);
+        byteBuff.flip();
+
+        utf8Charset.newDecoder().decode(byteBuff, charBuff, true);
+        charBuff.flip();
+
+        var charArr = new char[charBuff.length()];
+        if (charBuff.hasRemaining()) {
+            charBuff.get(charArr);
+            System.out.println("here: " + Arrays.toString(charArr));
+        }
+
+        System.out.println("Byte buffer limit - position = " + (byteBuff.limit() - byteBuff.position()));
+        bytes = Arrays.copyOfRange(byteBuff.array(), byteBuff.position(), byteBuff.limit());
+        System.out.println(Arrays.toString(bytes));
     }
 
     private static String filePath() {
